ce06b55e95a448bb444224af5675593234a959a9
Vicki Le <vicki@vicnle.com>
2016-01-05 20:53:31 -0500

    removes seconds from event times

--- layout/js/events.js
+++ layout/js/events.js
@@ -15,7 +15,7 @@ $(function(){
 					var day = dayMap[date.getDay()];
 					var event = '<a class=\'eventItem\' data-notes=\'links to the meetup page\' href=\'#\' title=\'EventTitle\'><span class=\'dateWrapper\'><span class=\'month\'>' + 
 						ev.month + '</span><span class=\'date\'>' + date.getDate()+ '</span></span><span class=\'eventWrapper\'><h4 class=\'eventTitle\'><span>' + 
-						ev.title + '</span></h4><span class=\'eventMeta\'>'+ day +' <span>at &nbsp;&nbsp;</span>' + date.toLocaleTimeString() + '</span></span></a>'
+						ev.title + '</span></h4><span class=\'eventMeta\'>'+ day +' <span>at &nbsp;&nbsp;</span>' + date.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}) + '</span></span></a>'
 					$events.append(event)
 				});
 
