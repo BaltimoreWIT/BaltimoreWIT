41c0b93fac128d44c0e68c4c02a09a4f97f77d8d
Angelique Weger <hello@angeliqueweger.com>
2016-02-19 06:26:18 -0500

    Updated events output to use actual event name in title tag.

--- layout/js/events.js
+++ layout/js/events.js
@@ -13,7 +13,7 @@ $(function(){
 				$.each(decodedResponse, function(i, ev) {
 					var date = new Date(ev.day.date);
 					var day = dayMap[date.getDay()];
-					var event = '<li><a class=\'eventItem\' data-notes=\'links to the meetup page\' href=\'#\' title=\'EventTitle\'><span class=\'dateWrapper\'><span class=\'month\'>' + 
+					var event = '<li><a class=\'eventItem\' data-notes=\'links to the meetup page\' href=\'#\' title=\'' + ev.title + '\'><span class=\'dateWrapper\'><span class=\'month\'>' + 
 						ev.month + '</span><span class=\'date\'>' + date.getDate()+ '</span></span><span class=\'eventWrapper\'><h4 class=\'eventTitle\'><span>' + 
 						ev.title + '</span></h4><span class=\'eventMeta\'>'+ day +' <span>at &nbsp;&nbsp;</span>' + date.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}) + '</span></span></a></li>'
 					$events.append(event)
